47b9e02f7077f231f806a43db0661b24e2f8022a
AlyfAhmed <alyahmed04@vt.edu>
2025-11-26 09:52:55 -0800

    added remove unit tests, and fixed comments for all functions

--- Database_Operations/UserDB.py
+++ Database_Operations/UserDB.py
@@ -7,6 +7,7 @@ from Models.Spot import Spot
 #https://www.w3schools.com/python/python_mysql_update.asp
 #https://www.red-gate.com/simple-talk/databases/mysql/modifying-mysql-data-from-within-python/
 
+
 create_user = ("INSERT INTO users "
                 "(username, email, hashedPassword, favoriteStudySpot, kudos) "
                 "VALUES (%s, %s, %s, %s, %s)")
@@ -34,5 +35,8 @@ class UserDB:
         execute_DBOperation(edit_favoriteStudySpot, value)
 
     def delete_user(user: User):
-        value = (user.userID)
+        #learned from:
+        #https://stackoverflow.com/questions/61831138/creating-python-tuple-with-one-int-item
+        value = tuple([user.userID,])
+        
         execute_DBOperation(delete_user, value)
--- main.py
+++ main.py
@@ -7,9 +7,6 @@ from Database_Operations.ReviewDB import ReviewDB
 import unittest
 import mysql.connector
 
-#learned about unit test order from 
-#https://stackoverflow.com/questions/4095319/unittest-tests-order
-unittest.TestLoader.sortTestMethodsUsing = None
 
 config = {
   'user': 'root',
@@ -27,22 +24,27 @@ config = {
 
 #SQL commands learned from under various pages
 #https://www.w3schools.com/python/python_mysql_where.asp
+#https://dev.mysql.com/doc/connector-python/en/connector-python-api-mysqlcursor-fetchone.html
 
 class test_SQL_commands(unittest.TestCase):        
 
+    #This method tests the creation of study spot in the SpotDB class
     def test_01_create_studyspot(self):
-        db = mysql.connector.connect(**config)
-        cursor = db.cursor()
+
         # Create a study spot
         print("1. Creating a study spot")
         SpotDB.create_spot("Newman Library", "560 Drillfield Dr, Blacksburg, VA")
 
+        db = mysql.connector.connect(**config)
+        cursor = db.cursor()
         cursor.execute("SELECT * FROM study_spots WHERE spotName = 'Newman Library'")
 
         #learned from:
         #https://www.geeksforgeeks.org/dbms/querying-data-from-a-database-using-fetchone-and-fetchall/
         result = cursor.fetchone()
 
+
+        #expected tuple from query
         expected = (1, "Newman Library", "560 Drillfield Dr, Blacksburg, VA")
         self.assertEqual(result[1], expected[1])
         self.assertEqual(result[2], expected[2])
@@ -55,11 +57,13 @@ class test_SQL_commands(unittest.TestCase):
         cursor.close()
         db.close()
 
+
+    #This method tests the creation of a user in the UserDB class
     def test_02_create_user(self):
+
+
         # Create a user
         print("2. Creating a user")
-        db = mysql.connector.connect(**config)
-        cursor = db.cursor()
         UserDB.create_user(
             username="testuser5",
             email="test5@vt.edu",
@@ -68,13 +72,15 @@ class test_SQL_commands(unittest.TestCase):
             kudos=0
         )
 
+        db = mysql.connector.connect(**config)
+        cursor = db.cursor()
         cursor.execute("SELECT * FROM users WHERE username = 'testuser5'")
 
         #learned from:
         #https://www.geeksforgeeks.org/dbms/querying-data-from-a-database-using-fetchone-and-fetchall/
         result = cursor.fetchone()
 
-        print(result)
+        #expected tuple from query
         expected = (1, "testuser5", "test5@vt.edu", "testPassword", None, 0)
         self.assertEqual(result[1], expected[1])
         self.assertEqual(result[2], expected[2])
@@ -91,9 +97,14 @@ class test_SQL_commands(unittest.TestCase):
         cursor.close()
         db.close()
 
-    def test_03_create_review(self):
-        print("3. Creating a review")
 
+    #This method tests the creation of a review in the ReviewDB class
+    def test_03_create_review(self):
+
+
+        print("3. Creating a review")
+        
+        #create a user to bind to a review
         UserDB.create_user(
             username="testuser2",
             email="test2@vt.edu",
@@ -102,6 +113,7 @@ class test_SQL_commands(unittest.TestCase):
             kudos=0
         )
 
+        #create a study spot to bind to a review
         SpotDB.create_spot("New", "580 Drillfield Dr, Blacksburg, VA")
 
         db = mysql.connector.connect(**config)
@@ -112,7 +124,6 @@ class test_SQL_commands(unittest.TestCase):
         #learned from:
         #https://www.geeksforgeeks.org/dbms/querying-data-from-a-database-using-fetchone-and-fetchall/
         userRes = cursor.fetchone()
-
         user = User(userID=userRes[0],username="testuser2",email="test2@vt.edu",password="testPassword",favoriteStudySpot=None,kudos=0)
 
         cursor.execute("SELECT * FROM study_spots WHERE spotName = 'new'")
@@ -120,7 +131,6 @@ class test_SQL_commands(unittest.TestCase):
         #learned from:
         #https://www.geeksforgeeks.org/dbms/querying-data-from-a-database-using-fetchone-and-fetchall/
         spotRes = cursor.fetchone()
-
         spot = Spot(spotID=spotRes[0],spotName="New",location="580 Drillfield Dr, Blacksburg, VA")
 
         cursor.close()
@@ -131,12 +141,16 @@ class test_SQL_commands(unittest.TestCase):
         db = mysql.connector.connect(**config)
         cursor = db.cursor()
 
-        cursor.execute("SELECT * FROM spot_reviews;")
+ 
+        #Syntax for and learned from:
+        #https://stackoverflow.com/questions/69960814/how-to-use-where-and-in-mysql-query-and-python
+        cursor.execute("SELECT * FROM spot_reviews WHERE userID = %s AND spotID = %s", (userRes[0], spotRes[0]))
 
         #learned from:
         #https://www.geeksforgeeks.org/dbms/querying-data-from-a-database-using-fetchone-and-fetchall/
         result = cursor.fetchone()
 
+        #Create the expected tuple from query
         expected = (1, spot.spotID, user.userID, 7, 7, 4, "Test content")
         self.assertEqual(result[1], expected[1])
         self.assertEqual(result[2], expected[2])
@@ -155,12 +169,13 @@ class test_SQL_commands(unittest.TestCase):
         cursor.close()
         db.close()
 
-    def test_04_edit_spot_name(self):
 
-        # Edit the name of a study spot
+    #This method tests the edit of a study spots name in the SpotDB class
+    def test_04_edit_spot_name(self):
 
         print("4. Editing a spot's name")
 
+         # Create a new study spot
         SpotDB.create_spot("Newman Library", "560 Drillfield Dr, Blacksburg, VA")
 
         db = mysql.connector.connect(**config)
@@ -176,6 +191,8 @@ class test_SQL_commands(unittest.TestCase):
 
         cursor.close()
         db.close()
+
+        # Edit the name of a study spot
         SpotDB.edit_spot_name(spot, "Newman")
 
         db = mysql.connector.connect(**config)
@@ -203,12 +220,13 @@ class test_SQL_commands(unittest.TestCase):
         cursor.close()
         db.close()
 
+
+    #This method tests the edit of a study spots location in the SpotDB class
     def test_05_edit_spot_location(self):
-
-        # Edit the name of a study spot
        
-        print("4. Editing a spot's name")
+        print("5. Editing a spot's location")
 
+        # Create new a study spot
         SpotDB.create_spot("man", "560 Drillfield Dr, Blacksburg, VA")
 
         db = mysql.connector.connect(**config)
@@ -223,6 +241,8 @@ class test_SQL_commands(unittest.TestCase):
 
         cursor.close()
         db.close()
+
+        # Edit the study spot's Location
         SpotDB.edit_spot_location(spot, "570 Drillfield Dr, Blacksburg, VA")
         
         db = mysql.connector.connect(**config)
@@ -233,6 +253,7 @@ class test_SQL_commands(unittest.TestCase):
         #https://www.geeksforgeeks.org/dbms/querying-data-from-a-database-using-fetchone-and-fetchall/
         result = cursor.fetchone()
 
+        #expected tuple from query
         expected = (spotRes[0], "man", "570 Drillfield Dr, Blacksburg, VA")
         self.assertEqual(result, expected)
 
@@ -244,7 +265,227 @@ class test_SQL_commands(unittest.TestCase):
 
         cursor.close()
         db.close()
+    
+
+    #This method tests the edit of a user's username in the UserDB class
+    def test_06_edit_username(self):
         
+        print("6. Edit a user's username")
+        
+        #Create a new user
+        UserDB.create_user(
+            username="testuser3",
+            email="test3@vt.edu",
+            password="testPassword",
+            favoriteStudySpot=None,
+            kudos=0
+        )
+
+        db = mysql.connector.connect(**config)
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM users WHERE username = 'testuser3'")
+        
+        #learned from:
+        #https://www.geeksforgeeks.org/dbms/querying-data-from-a-database-using-fetchone-and-fetchall/
+        userRes = cursor.fetchone()
+
+       
+        user = User(userID=userRes[0], username="testuser3",email="test3@vt.edu",password="testPassword",favoriteStudySpot=None,kudos=0) 
+
+        cursor.close()
+        db.close()
+
+        #Edit the user's username
+        UserDB.edit_username(user, "EditUserNameTest")
+
+        db = mysql.connector.connect(**config)
+        cursor = db.cursor()
+
+        #learned from:
+        #https://stackoverflow.com/questions/61831138/creating-python-tuple-with-one-int-item
+        value = tuple([userRes[0],])
+
+        cursor.execute("SELECT * FROM users WHERE userID = %s", value)
+
+        result = cursor.fetchone()
+
+        #Expeted tuple from query
+        expected = (userRes[0], "EditUserNameTest", "test3@vt.edu", "testPassword", None, 0)
+        
+        self.assertEqual(result, expected)
+        
+        #learned from:
+        #https://www.w3schools.com/python/python_mysql_delete.asp
+        cursor.execute("DELETE FROM users WHERE userID = %s", value)
+
+        db.commit()
+
+        cursor.close()
+        db.close()
+    
+
+    #This method tests the edit of a user's favorite study spit in the UserDB class
+    def test_07_edit_favorite_spot(self):
+
+        print("7. Edit Favorite Spot")
+        
+        #Create a new user
+        UserDB.create_user(
+            username="testuser4",
+            email="test4@vt.edu",
+            password="testPassword",
+            favoriteStudySpot=None,
+            kudos=0
+        )
+
+        #Create a new study spot
+        SpotDB.create_spot("Newman Library", "560 Drillfield Dr, Blacksburg, VA")
+
+        db = mysql.connector.connect(**config)
+        cursor = db.cursor()
+
+        cursor.execute("SELECT * FROM study_spots WHERE spotName = 'Newman Library'")
+
+        #learned from:
+        #https://www.geeksforgeeks.org/dbms/querying-data-from-a-database-using-fetchone-and-fetchall/
+        spotRes = cursor.fetchone()
+
+        spot = Spot(spotID=spotRes[0],spotName="Newman Library",location="560 Drillfield Dr, Blacksburg, VA")
+
+        cursor.execute("SELECT * FROM users WHERE username = 'testuser4'")
+        
+        #learned from:
+        #https://www.geeksforgeeks.org/dbms/querying-data-from-a-database-using-fetchone-and-fetchall/
+        userRes = cursor.fetchone()
+
+       
+        user = User(userID=userRes[0], username="testuser4",email="test4@vt.edu",password="testPassword",favoriteStudySpot=None,kudos=0) 
+
+        cursor.close()
+        db.close()
+
+        #Edit the users favorite study spot
+        UserDB.edit_favorite_study_spot(user, spot)
+
+        db = mysql.connector.connect(**config)
+        cursor = db.cursor()
+
+        #learned from:
+        #https://stackoverflow.com/questions/61831138/creating-python-tuple-with-one-int-item
+        userId = tuple([userRes[0],])
+        spotId = tuple([spotRes[0],])
+
+
+        cursor.execute("SELECT * FROM users WHERE userID = %s", userId)
+
+        result = cursor.fetchone()
+
+        #Expected tuple from query
+        expected = (userRes[0], "testuser4", "test4@vt.edu", "testPassword", spotRes[0], 0)
+        
+        self.assertEqual(result, expected)
+        
+
+        #learned from:
+        #https://www.w3schools.com/python/python_mysql_delete.asp
+        cursor.execute("DELETE FROM users WHERE userID = %s", userId)
+        cursor.execute("DELETE FROM study_spots WHERE spotID = %s", spotId)
+
+        db.commit()
+
+        cursor.close()
+        db.close()
+    
+
+     #This method tests the removal of a user in the UserDB class
+    def test_08_remove_user(self):
+
+        print("8. Remove User")
+
+        #Create a new user
+        UserDB.create_user(
+            username="testuser6",
+            email="test6@vt.edu",
+            password="testPassword",
+            favoriteStudySpot=None,
+            kudos=0
+        )
+
+        db = mysql.connector.connect(**config)
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM users WHERE username = 'testuser6'")
+        
+        #learned from:
+        #https://www.geeksforgeeks.org/dbms/querying-data-from-a-database-using-fetchone-and-fetchall/
+        userRes = cursor.fetchone()
+        user = User(userID=userRes[0], username="testuser6",email="test6@vt.edu",password="testPassword",favoriteStudySpot=None,kudos=0) 
+
+        cursor.close()
+        db.close()
+
+        #Remove the user
+        UserDB.delete_user(user)
+
+        db = mysql.connector.connect(**config)
+        cursor = db.cursor()
+
+        cursor.execute("SELECT * FROM users WHERE username = 'testuser6'")
+
+        #learned from:
+        #https://www.geeksforgeeks.org/dbms/querying-data-from-a-database-using-fetchone-and-fetchall/
+        removedUserRes = cursor.fetchone()
+
+        self.assertIsNone(removedUserRes)
+
+        cursor.close()
+        db.close()
+    
+
+    #This method tests the removal of a study spot in the SpotDB class
+    def test_09_remove_spot(self):
+
+        print("8. Remove Study Spot")
+
+        #Create a new study spot
+        SpotDB.create_spot("Newman Library", "560 Drillfield Dr, Blacksburg, VA")
+
+        db = mysql.connector.connect(**config)
+        cursor = db.cursor()
+
+        cursor.execute("SELECT * FROM study_spots WHERE spotName = 'Newman Library'")
+
+        #learned from:
+        #https://www.geeksforgeeks.org/dbms/querying-data-from-a-database-using-fetchone-and-fetchall/
+        spotRes = cursor.fetchone()
+    
+        spot = Spot(spotID=spotRes[0],spotName="Newman Library",location="560 Drillfield Dr, Blacksburg, VA")
+
+        cursor.close()
+        db.close()
+
+        #Remove the study spot
+        SpotDB.delete_spot(spot)
+
+        db = mysql.connector.connect(**config)
+        cursor = db.cursor()
+
+        #learned from:
+        #https://stackoverflow.com/questions/61831138/creating-python-tuple-with-one-int-item
+        spotId = tuple([spotRes[0],])
+
+        cursor.execute("SELECT * FROM study_spots WHERE spotID = %s", spotId)
+
+        #learned from:
+        #https://www.geeksforgeeks.org/dbms/querying-data-from-a-database-using-fetchone-and-fetchall/
+        removedSpotRes = cursor.fetchone()
+
+        self.assertIsNone(removedSpotRes)
+
+        cursor.close()
+        db.close()
+
+
+
 
 if __name__ == "__main__":
     unittest.main()
