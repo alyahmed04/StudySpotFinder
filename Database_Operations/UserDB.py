fe199bf193d73b199f680d593a6c55cba50e7a04
AlyfAhmed <alyahmed04@vt.edu>
2025-11-26 10:12:28 -0800

    added a function that updates the users kudos value, and added comments in the UserDB class

--- Database_Operations/UserDB.py
+++ Database_Operations/UserDB.py
@@ -14,10 +14,13 @@ create_user = ("INSERT INTO users "
 
 edit_username = "UPDATE users SET username = %s WHERE userID = %s"
 edit_favoriteStudySpot = "UPDATE users SET favoriteStudySpot = %s WHERE userID = %s"
+update_kudos = "UPDATE users SET kudos = %s WHERE userID = %s"
 delete_user = "DELETE FROM users WHERE userID = %s"
 
 class UserDB:
 
+
+    #This function creates a user in the database
     def create_user(username: str, email: str, password: str, favoriteStudySpot: str, kudos: int = 0, spot: Spot = None):
         if spot is None:
             value = (username, email, password, spot, kudos)
@@ -25,14 +28,23 @@ class UserDB:
             value = (username, email, password, spot.spotID, kudos)
         execute_DBOperation(create_user, value)
 
+
+    #This function edits a users username in the database
     def edit_username(user: User, username: str):
         value = (username, user.userID)
         execute_DBOperation(edit_username, value)
 
+    #This function edits a users favorite study spot in the database
     def edit_favorite_study_spot(user: User, spot: Spot):
         value = (spot.spotID, user.userID)
         execute_DBOperation(edit_favoriteStudySpot, value)
+    
+    #This function updates the kudos of the user by passing in the updated number of kudos
+    def update_kudos(user: User, count: int):
+        value = (count, user.userID)
+        execute_DBOperation(update_kudos, value)
 
+    #This function removes users from the database based on their userId
     def delete_user(user: User):
         value = (user.userID,)
         execute_DBOperation(delete_user, value)
